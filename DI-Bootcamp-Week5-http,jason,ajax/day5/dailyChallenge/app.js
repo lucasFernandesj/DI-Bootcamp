279e57d4cf22347d9b30e41d225a091daa260fa0
lucasFernandesj <fernandes.lucas.jo@gmail>
2022-04-14 14:35:44 +0300

    week5-day5

---
+++ DI-Bootcamp-Week5-http,jason,ajax/day5/dailyChallenge/app.js
@@ -0,0 +1,37 @@
+
+
+
+document.querySelector('.convert').addEventListener('click' , async()=>{
+    let to = document.querySelector('#to').value ;
+    let number = document.querySelector('#number').value;
+    let curr = document.querySelector('#from').value
+    
+
+    if(to ==='' ||number==='' ||curr==='' ){
+        alert('Check values')
+
+    }else{
+        try{
+    let data = await fetch(`https://v6.exchangerate-api.com/v6/4b15e8d0eb3326f5e043117c/latest/${curr}`)
+    let dataJson = await data.json()
+    let result = dataJson.conversion_rates
+    console.log(result)
+
+
+    let res = result[to] * number;
+
+    document.querySelector('.res').innerText = res; 
+        }catch(err){
+            console.log(err)
+            console.log('*****CATCHED*****')
+        }
+     
+    }
+    
+
+})
+
+
+
+
+
