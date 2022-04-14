e8c47c8ecb9e94457e6c13e9e252ea08d521e074
lucasFernandesj <fernandes.lucas.jo@gmail>
2022-04-14 13:46:18 +0300

    week5-day5

---
+++ DI-Bootcamp-Week5-http,jason,ajax/app.js
@@ -0,0 +1,49 @@
+document.querySelector('.btn').addEventListener('click' , async()=>{
+    displayLoading()
+    let thisName = document.querySelector('#name').value 
+    let currChar;
+    try{
+        const res = await fetch('https://www.swapi.tech/api/people/');
+        const resFinal = await res.json();
+        const arrChar = resFinal.results
+        console.log(arrChar)
+        arrChar.forEach(element => {
+            if(element.name === thisName){
+                
+                currChar = element
+                console.log(currChar)
+            }
+        });
+        let div = document.createElement('div') 
+        let p = document.createElement('p') 
+        p.innerText = currChar.name;
+        div.appendChild(p);
+        document.querySelector('.container').appendChild(div)
+        hideLoading()
+
+
+
+
+
+
+
+
+    }catch(err){
+        console.log(err)
+        console.log('*****CATCHED*****')
+    }
+  
+
+})
+
+
+
+const displayLoading =()=>{
+    document.querySelector('.loading').setAttribute('id' , '')
+
+}
+
+const hideLoading = ()=>{
+    document.querySelector('.loading').setAttribute('id' , 'hidden')
+
+}
